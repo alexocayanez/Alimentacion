f7ebb596d8098ab1ff614aeb0aa621bdb3c647fb
ziP3Rz <94759675+ziP3Rz@users.noreply.github.com>
2023-10-24 00:42:51 +0200

    Initial Commit
    Inicialización SpringBoot y archivo .drawio con el MER

---
+++ application/demo/src/test/java/net/apps/demo/ApplimentacionApplicationTests.java
@@ -0,0 +1,13 @@
+package net.apps.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplimentacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
