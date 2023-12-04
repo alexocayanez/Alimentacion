9ef55b574dd3ca05f2b8abb74a61a08f0ffc6f2e
ziP3Rz <94759675+ziP3Rz@users.noreply.github.com>
2023-12-04 22:36:19 +0100

    New project

---
+++ alimentacion/alimentacion-api/src/main/java/net/jaa/alimentacion/api/boot/AlimentacionApplication.java
@@ -0,0 +1,18 @@
+package net.jaa.alimentacion.api.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@ComponentScan(basePackages = "alimentacion")
+@ServletComponentScan(basePackages = "alimentacion")
+public class AlimentacionApplication extends SpringBootServletInitializer{
+	public static void main(String[] args) {
+		SpringApplication.run(AlimentacionApplication.class, args);
+	}
+}
